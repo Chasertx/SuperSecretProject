32cb1426e8ef5a1beff89e22bc9306e0caf2185f
chasertx <chaseashby199@gmail.com>
2026-02-04 11:44:47 -0600

    updated cores

--- Program.cs
+++ Program.cs
@@ -99,7 +99,8 @@ builder.Services.AddCors(options =>
         policy.WithOrigins(
             "http://localhost:5173",
             "https://lemon-stone-000a82c1e.2.azurestaticapps.net",
-            "https://orange-bush-0810b1a1e.1.azurestaticapps.net"
+            "https://orange-bush-0810b1a1e.1.azurestaticapps.net",
+            "www.therealchaseashby.com"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
